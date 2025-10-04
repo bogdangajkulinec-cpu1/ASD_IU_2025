ffcebcd49b7a1473b31bfa3262333d7f98079239
bogdangajkulinec-cpu1 <bogdangajkulinec@gmail.com>
2025-10-04 13:27:46 +0300

    Task3GroupB.java

--- lab/lab1/Task3GroupB.java
+++ lab/lab1/Task3GroupB.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class SumOfTwoSmallestInArray {
+public class Task3GroupB {
     public static void main(String[] args) {
         int array [] = inputArray();
         sumOfTwoSmallestInArray(array);
