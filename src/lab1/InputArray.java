b7ee89f02e2e0711297822bebee109d406958356
bogdangajkulinec-cpu1 <bogdangajkulinec@gmail.com>
2025-10-04 15:27:38 +0300

    bogdangajkulinec-cpu1

---
+++ src/lab1/InputArray.java
@@ -0,0 +1,21 @@
+package lab1;
+
+import java.util.Scanner;
+
+public class InputArray {
+    public static void main(String[] args) {
+    }
+    public static int[] inputArray() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите длину массива: ");
+        int length = scanner.nextInt();
+        int[] array = new int[length];
+        System.out.println("Введите " + length + " элементов массива:");
+        for (int i = 0; i < length; i++) {
+            System.out.print("Элемент [" + i + "]: ");
+            array[i] = scanner.nextInt();
+        }
+        return array;
+    }
+
+}
